0ec66b17bdb2932fda580577e9b37cad75136ce5
Maxime <maxime.dharboulle@gmail.com>
2022-02-14 23:24:16 +0100

    Add compression

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-
+import * as compression from 'compression';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.use(compression());
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({
