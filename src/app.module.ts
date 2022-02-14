f51c1cc734feeefe8747dde8605240e57ecc16cc
Maxime <maxime.dharboulle@gmail.com>
2022-02-14 23:05:18 +0100

    init

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { RootModule } from './root/root.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['envs/.env', 'envs/.env.staging', 'envs/.env.production'],
+      isGlobal: true,
+    }),
+    RootModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/root/root.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Controller()
+export class RootController {
+  constructor(private config: ConfigService) {}
+
+  @Get()
+  hello(): string {
+    // eslint-disable-next-line prettier/prettier
+    return `Welcome to the ${this.config.get<string>('NAME')} api in ${this.config.get<string>('ENV_NAME')}.`;
+  }
+}
