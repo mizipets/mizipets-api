629815ca2f0cf25e5246ffd5d9555284f6bfc3c4
Maxime <maxime.dharboulle@gmail.com>
2022-02-16 21:49:36 +0100

    Add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 import { RootModule } from './root/root.module';
 
@@ -9,9 +11,18 @@ import { RootModule } from './root/root.module';
       envFilePath: ['envs/.env', 'envs/.env.staging', 'envs/.env.production'],
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 20,
+    }),
     RootModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
