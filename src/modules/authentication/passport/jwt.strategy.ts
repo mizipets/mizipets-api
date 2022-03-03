1827285eb9e66b29bdeac62865825b8f6b6cb951
Juliien <julien1105@outlook.fr>
2022-03-03 21:53:09 +0100

    fix: package.json and env name

--- src/modules/authentication/passport/jwt.strategy.ts
+++ src/modules/authentication/passport/jwt.strategy.ts
@@ -4,23 +4,25 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import {UsersService} from "../../users/users.service";
 import {User} from "../../users/user.entity";
 import {JwtPayloadDto} from "../dto/jwt-payload.dto";
+import {ConfigService} from "@nestjs/config";
 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private usersService: UsersService) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: process.env.ENV === 'local',
-      secretOrKey: process.env.JWT_SECRET,
-    });
-  }
+    constructor(private usersService: UsersService,
+                private config: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: process.env.ENV_NAME === 'local',
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
 
-  async validate(payload: JwtPayloadDto): Promise<User> {
-    const user: User = await this.usersService.getById(payload.id);
-    if (!user)
-      throw new UnauthorizedException('Invalid token.');
+    async validate(payload: JwtPayloadDto): Promise<User> {
+        const user: User = await this.usersService.getById(payload.id);
+        if (!user)
+            throw new UnauthorizedException('Invalid token.');
 
-    return user;
-  }
+        return user;
+    }
 }
