eae96917a3b3166144ec1005b467e6d72f37545f
Maxime <maxime.dharboulle@gmail.com>
2022-03-16 12:14:46 +0100

    feat: activate or deactivate services

--- src/modules/services/services.controller.ts
+++ src/modules/services/services.controller.ts
@@ -5,7 +5,16 @@
  * @modify date 2022-03-16 00:35:16
  * @desc [description]
  */
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Put
+} from '@nestjs/common';
+import { Roles } from '../authentication/enum/roles.emum';
+import { OnlyRoles } from '../authentication/guards/role.decorator';
 import { ServicesService } from './services.service';
 
 @Controller('services')
@@ -16,5 +25,19 @@ export class ServicesController {
     @HttpCode(HttpStatus.OK)
     public async getAll() {
         return await this.servicesService.getAll();
+    }
+
+    @Put(':id/activate')
+    @HttpCode(HttpStatus.OK)
+    @OnlyRoles(Roles.ADMIN)
+    public async activate(@Param('id') id: string) {
+        return await this.servicesService.activate(parseInt(id));
+    }
+
+    @Put(':id/deactivate')
+    @HttpCode(HttpStatus.OK)
+    @OnlyRoles(Roles.ADMIN)
+    public async deactivate(@Param('id') id: string) {
+        return await this.servicesService.deactivate(parseInt(id));
     }
 }
