74fc1d9fbb9a2a9689c34a3c83840ac347c3d3ad
Maxime <maxime.dharboulle@gmail.com>
2022-03-17 17:34:13 +0100

    Revert "test"
    This reverts commit 2047814a57b538ca0dee75f0d4e0d6a978a2a63f.

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const origins = ['http://localhost'];
 
-    
     app.enableCors({ origin: origins });
     app.use(compression());
     app.use(helmet());
