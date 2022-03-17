850aab844468ec4b32cec7009fcf039f27112a78
Maxime <maxime.dharboulle@gmail.com>
2022-03-17 10:05:49 +0100

    WIP: remove favorite

--- src/modules/favorites/favorites.controller.ts
+++ src/modules/favorites/favorites.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { Roles } from '../authentication/enum/roles.emum';
 import { OnlyRoles } from '../authentication/guards/role.decorator';
+import { ServiceType } from '../services/enums/service-type.enum';
 import { FavoritesService } from './favorites.service';
 
 @Controller('favorites')
@@ -11,5 +12,19 @@ export class FavoritesController {
     @OnlyRoles(Roles.STANDARD)
     async getFavoritesOfUser(@Param('userId') userId: string) {
         return await this.favoritesService.getFavoritesOfUser(parseInt(userId));
+    }
+
+    @Delete(':userId/:type/:referenceID')
+    @OnlyRoles(Roles.STANDARD)
+    async removeFavorite(
+        @Param('userId') userId: string,
+        @Param('type') type: ServiceType,
+        @Param('referenceID') referenceID: string
+    ) {
+        return await this.favoritesService.removeFavorite(
+            parseInt(userId),
+            type,
+            parseInt(referenceID)
+        );
     }
 }
