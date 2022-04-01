76797315252de612def1ac998a6e7afdabf80e4d
Juliien <julien1105@outlook.fr>
2022-04-01 12:01:48 +0200

    fix: room / messages module

--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -2,7 +2,7 @@
  * @author Maxime D'HARBOULLE
  * @create 2022-02-25
  */
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Animal } from '../animals/entities/animal.entity';
@@ -16,27 +16,33 @@ export class RoomService {
         @InjectRepository(Room) private readonly repository: Repository<Room>
     ) {}
 
-    async create(user: User, animal: Animal) {
+    async create(user: User, animal: Animal): Promise<void> {
         const room = new Room();
         room.adoptant = user;
         room.messages = [];
         room.animal = animal;
         room.code = room.getCode();
+
         await this.repository.save(room);
     }
 
-    async getById(id: number) {
-        return await this.repository.findOneOrFail(id);
+    async getById(id: number): Promise<Room> {
+        const room: Room = await this.repository.findOne(id);
+        if (!room)
+            throw new NotFoundException(`Room with id: ${id} not found`);
+
+        return room
     }
 
-    async writeMessage(id: number, text: string, userId: number) {
+    async writeMessage(id: number, text: string, userId: number): Promise<Room> {
         const message = new Message();
         message.created = new Date();
         message.text = text;
         message.writer = userId;
 
         const room = await this.getById(id);
+
         room.messages.push(message);
-        return await this.repository.save(room);
+        return this.repository.save(room);
     }
 }
