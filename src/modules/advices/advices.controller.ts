0e040e1d4741a6a16d77845ee5a4a1c9f2fc366a
Maxime <maxime.dharboulle@gmail.com>
2022-05-21 22:53:58 +0200

    Add: get advices & get advices by specie

--- src/modules/advices/advices.controller.ts
+++ src/modules/advices/advices.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { FindConditions } from 'typeorm';
+import { AdvicesService } from './advices.service';
+import { Advice } from './entities/advices.entity';
 
 @Controller('advices')
-export class AdvicesController {}
+export class AdvicesController {
+    constructor(private advicesService: AdvicesService) {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    async getAll() {
+        return await this.advicesService.getAll();
+    }
+
+    @Get('specie/:id')
+    @HttpCode(HttpStatus.OK)
+    async getBySpecie(@Param('id') id: number) {
+        const where: FindConditions<Advice> = {
+            specie: {
+                id: id
+            }
+        };
+        return await this.advicesService.getBy(where);
+    }
+}
--- src/modules/advices/advices.service.ts
+++ src/modules/advices/advices.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindConditions, Repository } from 'typeorm';
+import { Advice } from './entities/advices.entity';
 
 @Injectable()
-export class AdvicesService {}
+export class AdvicesService {
+    constructor(
+        @InjectRepository(Advice)
+        private readonly repository: Repository<Advice>
+    ) {}
+
+    async getAll() {
+        return await this.repository.find();
+    }
+
+    async getById(id: number) {
+        return await this.repository.findOne({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    async getBy(where: FindConditions<Advice>) {
+        return await this.repository.find({
+            where: where
+        });
+    }
+}
