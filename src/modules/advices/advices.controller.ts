ded281fc4ba0cc3b3ed0724bbd9bd1f7fde83884
Maxime <maxime.dharboulle@gmail.com>
2022-05-23 01:31:24 +0200

    Add: advice tips, & get advices by types

--- src/modules/advices/advices.controller.ts
+++ src/modules/advices/advices.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { FindConditions } from 'typeorm';
+import { Roles } from '../authentication/enum/roles.emum';
+import { OnlyRoles } from '../authentication/guards/role.decorator';
 import { AdvicesService } from './advices.service';
 import { Advice } from './entities/advices.entity';
+import { AdviceType } from './enums/advice-type.enum';
 
 @Controller('advices')
 export class AdvicesController {
@@ -9,17 +12,29 @@ export class AdvicesController {
 
     @Get()
     @HttpCode(HttpStatus.OK)
+    @OnlyRoles(Roles.PRO, Roles.STANDARD, Roles.ADMIN)
     async getAll() {
         return await this.advicesService.getAll();
     }
 
     @Get('specie/:id')
     @HttpCode(HttpStatus.OK)
+    @OnlyRoles(Roles.PRO, Roles.STANDARD, Roles.ADMIN)
     async getBySpecie(@Param('id') id: number) {
         const where: FindConditions<Advice> = {
             specie: {
                 id: id
             }
+        };
+        return await this.advicesService.getBy(where);
+    }
+
+    @Get('type/:type')
+    @HttpCode(HttpStatus.OK)
+    @OnlyRoles(Roles.PRO, Roles.STANDARD, Roles.ADMIN)
+    async getByType(@Param('type') type: AdviceType) {
+        const where: FindConditions<Advice> = {
+            type: type
         };
         return await this.advicesService.getBy(where);
     }
--- src/modules/advices/enums/advice-type.enum.ts
+++ src/modules/advices/enums/advice-type.enum.ts
@@ -1,5 +1,6 @@
 export enum AdviceType {
     VIDEO = 'VIDEO',
     IMAGE = 'IMAGE',
+    TIP = 'TIP',
     TEXT = 'TEXT'
 }
