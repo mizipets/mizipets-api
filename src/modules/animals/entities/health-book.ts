7c2c54709a516a6f90ce55253f6e978928ba5ba3
Maxime <maxime.dharboulle@gmail.com>
2022-05-26 22:48:08 +0200

    Add: wip

--- src/modules/animals/entities/health-book.ts
+++ src/modules/animals/entities/health-book.ts
@@ -1,1 +1,13 @@
-export class HealthBook {}
+export class HealthBook {
+    vaccins: Vaccin[]
+}
+
+
+export class Vaccin implements Reminder {
+
+}
+
+export interface Reminder {
+    reccurent: boolean;
+    range: 
+}
