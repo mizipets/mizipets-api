7ad79d6929d8f95cf0ab2cdc4fbb86ebc2620a45
Maxime <maxime.dharboulle@gmail.com>
2022-05-31 23:52:44 +0200

    Fix: clean

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -2,12 +2,10 @@
  * @author Maxime D'HARBOULLE
  * @create 2022-05-28
  */
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { SendNotificationDTO } from './dto/send-notification.dto';
+import { Controller } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 
 @Controller('notifications')
 export class NotificationsController {
     constructor(private readonly notificationsService: NotificationsService) {}
-
 }
